dfa814e045df58d579bb2c112777d4d4589a07e4
Dexter Stephens <dexterfstephens@gmail.com>
2022-11-11 11:31:21 -0700

    Create the base setup for the project

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Money Manager</h1>
+    </div>
+  );
+}
+
+export default App;
